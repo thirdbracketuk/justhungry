2eab8c03b52c2364eec680d07db034b0af9bd636
sagarmusabbir <sagarmusabbir@gmail.com>
2026-07-06 22:57:51 +0600

    forgot-to-save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./index.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://justhungry.co.uk"),
   title: "Just Hungry",
   description: "Just Hungry - Fast food restaurant in Blackpool",
   other: {
