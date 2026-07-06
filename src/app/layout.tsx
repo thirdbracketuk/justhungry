9ef264d5d6166c55573355b26be2b1b47169f353
sagarmusabbir <sagarmusabbir@gmail.com>
2026-07-06 22:39:25 +0600

    og-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,26 @@
 import type { Metadata } from "next";
-import "../index.css";
+import "./index";
 
 export const metadata: Metadata = {
   title: "Just Hungry",
   description: "Just Hungry - Fast food restaurant in Blackpool",
   other: {
     "google-site-verification": "MdPN6W0dvA0WD2qBUFQsqcbkWQOcDctfX8IUd3PCCXc",
+  },
+  openGraph: {
+    title: "Just Hungry",
+    description: "Just Hungry - Fast food restaurant in Blackpool",
+    url: "https://www.justhungry.co.uk/",
+    siteName: "Just Hungry",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_GB",
+    type: "website",
   },
 };
 
